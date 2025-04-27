d48b6fa6363c4453b0131ac0a07cac3e41ee040a
XG3R4RD0X <gerardo.vasquez.p@gmail.com>
2025-04-27 17:43:06 +0200

    removed innecesary files

--- app/view/menu_view.py
+++ app/view/menu_view.py
@@ -24,27 +24,6 @@ class MainMenuView(BaseView):
         )
         self.update_and_download_button.pack(fill="x", pady=5)
 
-        self.check_database_button = ttk.Button(
-            self.buttons_frame,
-            text="Revisar base de datos actual",
-            command=self.check_database,
-        )
-        self.check_database_button.pack(fill="x", pady=5)
-
-        self.update_database_button = ttk.Button(
-            self.buttons_frame,
-            text="Actualizar base de datos",
-            command=self.update_songs_database,
-        )
-        self.update_database_button.pack(fill="x", pady=5)
-
-        self.download_with_database_button = ttk.Button(
-            self.buttons_frame,
-            text="Descargar canciones con base de datos actual",
-            command=self.download_with_database,
-        )
-        self.download_with_database_button.pack(fill="x", pady=5)
-
         self.view_database_button = ttk.Button(
             self.buttons_frame,
             text="Ver base de datos",
@@ -55,18 +34,6 @@ class MainMenuView(BaseView):
     def update_and_download(self):
         """Trigger the 'update_and_download' event."""
         self.trigger_event("update_and_download")
-
-    def check_database(self):
-        """Trigger the 'check_database' event."""
-        self.trigger_event("check_database")
-
-    def update_songs_database(self):
-        """Trigger the 'update_songs_database' event."""
-        self.trigger_event("update_songs_database")
-
-    def download_with_database(self):
-        """Trigger the 'download_with_database' event."""
-        self.trigger_event("download_with_database")
 
     def view_database(self):
         """Trigger the 'view_database' event."""
